40be416b83a5e64867eedbb3f270a4afc66e770c
adeto <adeto@users.noreply.github.com>
2017-01-21 14:59:57 +0200

    Strings and Regular Expression

---
+++ 01. Split a String with a Delimiter.js
@@ -0,0 +1,7 @@
+function main([string, delimeter]) {
+    let splitElements = string.split(delimeter);
+    for (let i = 0; i < splitElements.length; i++){
+    console.log(splitElements[i]);
+    }
+}
+
---
+++ 02. Repeat a String N Times.js
@@ -0,0 +1,10 @@
+function main([input]){
+    let  str = ([input]);
+    let nums = Number([input]);
+
+    String repeated = str.repeat(nums);
+
+
+}
+
+main(['repeatt, 5']);
